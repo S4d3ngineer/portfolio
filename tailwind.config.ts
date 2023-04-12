7311f73756e4971476587417f82cd53e60533425
S4d3ngineer <adam.arkuszynski@gmail.com>
2023-04-12 20:36:08 +0200

    style: change breakpoints unit from px to rem

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,13 @@ export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
+    screens: {
+      sm: `${640 / 16}rem`,
+      md: `${768 / 16}rem`,
+      lg: `${1024 / 16}rem`,
+      xl: `${1280 / 16}rem`,
+      "2xl": `${1536 / 16}rem`,
+    },
     extend: {},
   },
   plugins: [],
