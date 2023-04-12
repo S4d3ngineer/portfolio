46456810c5c85c0030ce82e2a77e6d20c7c65cd8
S4d3ngineer <adam.arkuszynski@gmail.com>
2023-04-12 18:46:58 +0200

    style: add fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,28 @@
 import { appWithTranslation } from "next-i18next";
 import { type AppType } from "next/dist/shared/lib/utils";
-import Layout from "~/components/layout";
+import Layout from "~/components/Layout";
 import { ThemeProvider } from "next-themes";
+import localFont from "next/font/local";
 
 import "~/styles/globals.css";
+
+const satoshi = localFont({
+  src: "../../public/Satoshi_Complete/Fonts/Variable/Satoshi-Variable.ttf",
+});
+
+const generalSans = localFont({
+  src: "../../public/GeneralSans_Complete/Fonts/Variable/GeneralSans-Variable.ttf",
+});
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ThemeProvider attribute="class">
       <Layout>
+        <style jsx global>{`
+          html {
+            font-family: ${generalSans.style.fontFamily};
+          }
+        `}</style>
         <Component {...pageProps} />
       </Layout>
     </ThemeProvider>
